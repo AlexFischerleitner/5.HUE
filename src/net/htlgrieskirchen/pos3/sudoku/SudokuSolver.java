9a340c02750f252ad5cafbc19ebeaf64dd0a0085
Fischerleitner Alex <fischerleitner03@gmail.com>
2020-10-19 13:27:34 +0200

    readSudoku

--- src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
+++ src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -1,6 +1,8 @@
 package net.htlgrieskirchen.pos3.sudoku;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /* Please enter here an answer to task four between the tags:
  * <answerTask4>
@@ -8,19 +10,39 @@ import java.io.File;
  * </answerTask4>
  */
 public class SudokuSolver implements ISodukoSolver {
+    private int [][] inputSudoku;
+    
     public SudokuSolver() {
-        //initialize if necessary
+        inputSudoku = new int[9][9];
     }
 
     @Override
     public final int[][] readSudoku(File file) {
-        // implement this method
-        return new int[0][0]; // delete this line!
+
+        try {
+            inputSudoku = Files.lines(file.toPath())
+                    .map(s -> s.split(";"))
+                    .map(a -> new int[]{Integer.parseInt(a[0]),
+                Integer.parseInt(a[1]),
+                Integer.parseInt(a[2]),
+                Integer.parseInt(a[3]),
+                Integer.parseInt(a[4]),
+                Integer.parseInt(a[5]),
+                Integer.parseInt(a[6]),
+                Integer.parseInt(a[7]),
+                Integer.parseInt(a[8])})
+                    .toArray(int[][]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inputSudoku;
     }
 
     @Override
     public boolean checkSudoku(int[][] rawSudoku) {
-        // implement this method
+        for (int i = 0; i <= 8; i++) {
+
+        }
         return false; // delete this line!
     }
 
@@ -29,7 +51,7 @@ public class SudokuSolver implements ISodukoSolver {
         // implement this method
         return new int[0][0]; // delete this line!
     }
-    
+
     @Override
     public int[][] solveSudokuParallel(int[][] rawSudoku) {
         // implement this method
